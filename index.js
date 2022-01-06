1ebf408fb4c6cb6640e3b02177ae9555145862e9
The-Six <ijazdriscoll@gmail.com>
2022-01-06 12:36:38 +0000

    remove item completed

--- index.js
+++ index.js
@@ -39,7 +39,14 @@ export function replaceItem(array, item, index) {
 }
 
 // Immutably remove an item at a specific position/index within an array
-export function removeItem(array, index) {}
+export function removeItem(array, index) {
+    const newArray = [
+        ...array.slice(0, index),
+
+        ...array.slice(index + 1)
+    ]
+        return newArray;
+}
 
 //Objects:
 
